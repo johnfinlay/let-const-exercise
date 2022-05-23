fc75e198171b8311e0da84090f405575ced62885
John Finlay <finlay_john@hotmail.com>
2022-05-23 07:17:45 -0500

    initial commit

---
+++ app.js
@@ -0,0 +1,13 @@
+// In this exercise, you’ll refactor some ES5 code into ES2015.
+
+// ES5 Global Constants
+var PI = 3.14;
+PI = 42; // stop me from doing this!
+// ES2015 Global Constants
+// /* Write an ES2015 Version */
+const PI = 3.14;
+// Quiz
+// What is the difference between var and let?
+// What is the difference between var and const?
+// What is the difference between let and const?
+// What is hoisting?
